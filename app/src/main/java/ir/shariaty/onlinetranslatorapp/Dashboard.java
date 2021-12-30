dbfa431aeab29cf21a96f755cfd64883907b4248
ElIcecrypt <aidashahrestani@gmail.com>
2021-12-30 13:51:54 +0330

    Create Translator UI

--- app/src/main/java/ir/shariaty/onlinetranslatorapp/Dashboard.java
+++ app/src/main/java/ir/shariaty/onlinetranslatorapp/Dashboard.java
@@ -1,8 +1,32 @@
 package ir.shariaty.onlinetranslatorapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +34,6 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
     }
 }
---
+++ app/src/main/java/ir/shariaty/onlinetranslatorapp/translateResult.java
@@ -0,0 +1,17 @@
+package ir.shariaty.onlinetranslatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class translateResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate_result);
+
+    }
+}
